2a3495f4cef5e6e6aae0e7e917cea425eb022942
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-02-22 07:17:58 -0500

    crear modelos iniciales y inicializar vista principal

--- myFinances/logBook/models.py
+++ myFinances/logBook/models.py
@@ -1,3 +1,19 @@
+from itertools import count
 from django.db import models
 
 # Create your models here.
+
+class account(models.Model):
+    name=models.CharField(max_length=50)
+
+class subaccount(models.Model):
+    name=models.CharField(max_length=50)
+    ledgerAccount=models.CharField(max_length=50)
+
+class accountingEvent(models.Model):
+    fecha=models.DateTimeField()
+    count=models.CharField(max_length=50)
+    subcount=models.CharField(max_length=50)
+    value=models.FloatField()
+    detail=models.CharField(max_length=150)
+
--- myFinances/logBook/views.py
+++ myFinances/logBook/views.py
@@ -1,6 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from logBook.models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, "logBook/index.html")
+    dataEvents=accountingEvent.objects.all()
+
+    context={
+        "dataEvents":dataEvents
+    }
+
+    return render(request, "logBook/index.html", context)
